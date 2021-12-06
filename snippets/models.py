166ebf6c74ca176c2f95438e1bee8a25aa1107fc
Aitor Lería Fortea <aitorleria@gmail.com>
2021-12-06 12:04:27 +0100

    Update models

--- snippets/models.py
+++ snippets/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100, blank=False)
     description = models.CharField(max_length=255, default='')
+
+class SnippetFile(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    content = models.TextField(max_length=25000, blank=False)
+    snippet = models.ForeignKey(
+        Snippet,
+        on_delete=CASCADE,
+        related_name='files',
+        related_query_name='file'
+    )
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from .models import Snippet
+from .models import Snippet, SnippetFile
+
+class SnippetFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SnippetFile
+        fields = ('id',
+                  'name',
+                  'content')
 
 class SnippetSerializer(serializers.ModelSerializer):
+    files = SnippetFileSerializer(many=True)
+
     class Meta:
         model = Snippet
         fields = ('id',
                   'name',
-                  'description')
+                  'description',
+                  'files')
