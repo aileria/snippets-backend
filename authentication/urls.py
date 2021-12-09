6c86e8145f9bb2f14892c4f63fb954d28ee2b6be
Aitor Lería Fortea <aitorleria@gmail.com>
2021-12-10 00:56:10 +0100

    Fix endpoint name

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,5 @@ from authentication.views import RegisterView
 urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
