39011f87ef9f36d6d4fa3d809bb2570fbb2ed75d
Aitor Lería Fortea <aitorleria@gmail.com>
2021-12-10 00:52:28 +0100

    Finish register endpoint

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,19 +1,33 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.models import User
+from rest_framework.fields import SerializerMethodField
+from rest_framework.validators import UniqueValidator
+from rest_framework import serializers
+from authentication.utils import get_user_tokens
 
 class RegisterSerializer(serializers.ModelSerializer):
+    token = SerializerMethodField()
     email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
+        required = True,
+        validators = [UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
+    def get_token(self, user):
+        return get_user_tokens(user)
+    
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = (
+            'username',
+            'password',
+            'password2',
+            'email',
+            'first_name',
+            'last_name',
+            'token'
+        )
 
     def validate(self, data):
         if data['password'] != data['password2']:
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls.conf import path
 from rest_framework_simplejwt import views as jwt_views
 
+from authentication.views import RegisterView
+
 urlpatterns = [
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='token_refresh'),
 ]
