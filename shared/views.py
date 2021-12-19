597e4f5f38bcda3feb53d0e82d7b83d2576a759d
Aitor Lería Fortea <aitorleria@gmail.com>
2021-12-19 19:01:55 +0100

    Create shared app

---
+++ shared/views.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets, permissions
+
+class BaseModelViewSet(viewsets.ModelViewSet):
+    queryset = ''
+    serializer_class = ''
+    permission_classes = (permissions.AllowAny,)
+
+    permission_classes_by_action = {
+        'create': permission_classes,
+        'list': permission_classes,
+        'retrieve': permission_classes,
+        'update': permission_classes,
+        'partial_update': permission_classes,
+        'destroy': permission_classes,
+    }
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            if self.action:
+                action_func = getattr(self, self.action, {})
+                action_func_kwargs = getattr(action_func, 'kwargs', {})
+                permission_classes = action_func_kwargs.get('permission_classes')
+            else:
+                permission_classes = None
+
+            return [permission() for permission in (permission_classes or self.permission_classes)]
