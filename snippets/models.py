ed544d952de9c1f62753780a00de0635d20e7163
Aitor Lería Fortea <aitorleria@gmail.com>
2021-12-29 17:39:03 +0100

    Add user-snippet relationship

--- snippets/models.py
+++ snippets/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Snippet(models.Model):
     name = models.CharField(max_length=100, blank=False)
     description = models.CharField(max_length=255, default='')
+    user = models.ForeignKey(
+        User,
+        on_delete=CASCADE,
+        related_name='snippets',
+        related_query_name='snippet'
+    )
 
 class SnippetFile(models.Model):
     name = models.CharField(max_length=100, blank=False)
