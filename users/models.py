55d489b6ce9a55ce9289d203d550c1a1cb12b31e
Aitor Lería Fortea <aitorleria@gmail.com>
2021-12-29 21:23:44 +0100

    Add IsOwner permission

--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
-    
--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from rest_framework import permissions
+from shared.permissions import IsOwner
 from shared.views import BaseModelViewSet
 from .serializers import UserSerializer
 from .models import User
 
+
 class UserViewSet(BaseModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
     permission_classes_by_action = {
-        "create": (permissions.IsAuthenticated,),
-        "update": (permissions.IsAuthenticated,),
-        'partial_update': (permissions.IsAuthenticated,),
-        "destroy": (permissions.IsAuthenticated,),
+        "create": (permissions.IsAdminUser | IsOwner,),
+        "update": (permissions.IsAdminUser | IsOwner,),
+        'partial_update': (permissions.IsAdminUser | IsOwner,),
+        "destroy": (permissions.IsAdminUser | IsOwner,),
     }
